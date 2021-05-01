927a974878b0c2bc7a4ee4683bbf656f444c4e50
Hisoka Morrow <61894183+HashT9u@users.noreply.github.com>
2021-05-01 16:40:13 -0400

    Update controller.js

--- JS/controller.js
+++ JS/controller.js
@@ -1,11 +1,9 @@
 $(function(){
     $('#button').click(() => {
-        let elements = ['#link', '#username', '#avatar', '#message']
-        let arr = [];
-        for (const e of elements){
-            let doc = $(`#${e}`)
-            arr.push(doc.val())
-        }
-        $.post(arr[0], {"content": arr[3], "username": arr[1], "avatar": arr[2]})
+        let link = $('#link').val()
+        let content = $('#content').val()
+        let username = $('#username').val()
+        let avatar = $('#avatar').val()
+        $.post(link, {"content": content, "username": content, "avatar": avatar})
     })
 });
